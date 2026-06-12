529cf0a4569e1c44426677d00ac2b0c4049156b0
aaza <dump.truck.aja@gmail.com>
2026-06-12 17:10:40 +0700

    fix: remove teh template of main menu

--- src/views/MainMenu/MainMenu.vue
+++ src/views/MainMenu/MainMenu.vue
@@ -1,17 +1,8 @@
 <script setup>
-import Button from "@/views/MainMenu/components/Button.vue"
+
 </script>
 
 
 <template>
-    <div>
 
-
-        <div>
-            <div class="">
-
-            </div>
-        </div>
-        <Button msg="Halo " />
-    </div>
 </template>
